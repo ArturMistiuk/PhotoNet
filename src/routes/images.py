d778b44304c4e23291286bc08a61575c31dc6ff2
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-08-05 20:59:53 +0200

    Update images.py

--- src/routes/images.py
+++ src/routes/images.py
@@ -28,12 +28,13 @@ access_admin = RolesAccess([Role.admin])
 async def get_images(db: Session = Depends(get_db),
                      current_user: User = Depends(auth_service.get_current_user)):
     """
-     Функція get_images повертає список зображень, які завантажив поточний користувач.
-     :param db: Сеанс: передає сеанс бази даних функції
-     :param current_user: Користувач: отримати інформацію про поточного користувача
-     :return: Список усіх зображень, пов’язаних із поточним користувачем
-     """
+The get_images function returns a list of images for the current user.
 
+:param db: Session: Pass the database session to the function
+:param current_user: User: Get the current user
+:return: A list of images that are associated with the current user
+
+    """
     user_images = await images.get_images(db, current_user)
     return {"images": user_images}
 
@@ -42,16 +43,14 @@ async def get_images(db: Session = Depends(get_db),
 async def get_image(id: int, db: Session = Depends(get_db),
                     current_user: User = Depends(auth_service.get_current_user)):
     """
-     Функція get_image повертає одне зображення з його оцінками та коментарями.
-     Функція приймає параметр ідентифікатора, який є ідентифікатором зображення, яке потрібно повернути.
-     Він також приймає параметр db, який використовується для доступу до бази даних. Поточний_користувач
-     Параметр використовується для автентифікації.
-     :param id: int: отримати зображення з цим ідентифікатором
-     :param db: Сеанс: доступ до бази даних
-     :param current_user: Користувач: отримати поточного користувача
-     :return: Dict із зображенням, оцінками та коментарями
-     """
+The get_image function returns a single image with its ratings and comments.
 
+:param id: int: Specify the id of the image that is being requested
+:param db: Session: Pass the database session to the get_image function
+:param current_user: User: Get the current user from the database
+:return: The image, the ratings and the comments
+
+    """
     user_image, ratings, comments = await images.get_image(db, id, current_user)
     return {"image": user_image, "ratings": ratings, "comments": comments}
 
@@ -60,15 +59,16 @@ async def get_image(id: int, db: Session = Depends(get_db),
 async def admin_get_images(user_id: int, db: Session = Depends(get_db),
                            current_user: User = Depends(auth_service.get_current_user)):
     """
-     Функція admin_get_images використовується для отримання всіх зображень для користувача.
-         Ця функція потребує user_id користувача, чиї зображення ви хочете отримати.
-         Параметр current_user необхідний для FastAPI та містить інформацію про адміністратора, який наразі ввійшов у систему.
-     :param user_id: int: Вкажіть user_id користувача, зображення якого потрібно отримати
-     :param db: Сеанс: доступ до бази даних
-     :param current_user: Користувач: перевірте, чи є користувач адміністратором
-     :return: Список усіх зображень користувача, які є в базі даних
-     """
+The admin_get_images function is used to get all images for a user.
+    This function requires the user_id of the user whose images you want to retrieve.
+    The current_user must be an admin in order to use this function.
 
+:param user_id: int: Get the images of a specific user
+:param db: Session: Access the database
+:param current_user: User: Get the current user who is making the request
+:return: A list of all images in the database
+
+    """
     user_response = await images.admin_get_image(db, user_id)
     return {"images": user_response}
 
@@ -78,19 +78,19 @@ async def admin_get_images(user_id: int, db: Session = Depends(get_db),
 async def upload_image(body: ImageAddModel = Depends(), file: UploadFile = File(), db: Session = Depends(get_db),
                        current_user: User = Depends(auth_service.get_current_user)):
     """
-     Функція upload_image використовується для завантаження зображення на хмарний сервер.
-         Функція приймає тіло, файл, сесію бази даних і поточного користувача як параметри.
-         Параметр body використовується для тегів і опису зображення, яке завантажується.
-         Параметр file використовується для завантаження фактичного файлу зображення з комп’ютера або пристрою користувача.
-         Параметр db session дозволяє отримати доступ до нашої бази даних за допомогою методів і функцій SQLAlchemy ORM.
-         Нарешті, ми використовуємо current_user, щоб ми могли пов’язати кожну завантажену фотографію з відповідним користувачем.
-     :param body: ImageAddModel: отримати опис і теги з тіла запиту
-     :param file: UploadFile: отримати файл із запиту
-     :param db: Сеанс: отримати підключення до бази даних із пулу
-     :param current_user: Користувач: отримати поточного користувача з бази даних
-     :return: Словник із зображенням і детальним повідомленням
-     """
+The upload_image function is used to upload an image to the database.
+    The function takes in a body, file, db, and current_user as parameters.
+    The body parameter is used for the ImageAddModel class which contains all of the information needed for adding an image.
+    The file parameter is used for uploading a file from your computer or device that you want to add as an image on PhotoNet.
+        This will be uploaded using Cloudinary's API and stored there until it needs to be retrieved by another user viewing your profile page or gallery page.
 
+:param body: ImageAddModel: Get the image's title, description and tags from the request body
+:param file: UploadFile: Get the file from the request body
+:param db: Session: Get the database session
+:param current_user: User: Get the current user from the database
+:return: The image and the details of the image
+
+    """
     cloudinary.config(
             cloud_name=os.environ.get('CLOUDINARY_NAME'),
             api_key=os.environ.get('CLOUDINARY_API_KEY'),
@@ -108,10 +108,6 @@ async def upload_image(body: ImageAddModel = Depends(), file: UploadFile = File(
     r = cloudinary.uploader.upload(file.file, public_id=f'PhotoNet/{file_name}', overwrite=True)
     src_url = cloudinary.CloudinaryImage(f'PhotoNet/{file_name}') \
         .build_url(width=250, height=250, crop='fill', version=r.get('version'))
-    # r = cloudinary.uploader.upload(file.file, public_id=f'PhotoShare/{file_name}', overwrite=True)
-    # src_url = cloudinary.CloudinaryImage(f'PhotoShare/{file_name}') \
-    #     .build_url(width=250, height=250, crop='fill', version=r.get('version'))
-
     image, details = await images.add_image(db, body, correct_tags, src_url, correct_public_name, current_user)
 
     return {"image": image, "detail": "Image was successfully added." + details}
@@ -122,16 +118,19 @@ async def upload_image(body: ImageAddModel = Depends(), file: UploadFile = File(
 async def update_description(image_id: int, image_info: ImageUpdateModel, db: Session = Depends(get_db),
                              current_user: User = Depends(auth_service.get_current_user)):
     """
-     Функція update_description оновлює опис зображення.
-         Функція приймає сеанс бази даних, current_user і image_id як параметри.
-         Потім він використовує функцію update_image із зображень, щоб оновити опис зображення з цим ідентифікатором.
-     :param image_id: int: Визначте зображення, яке оновлюється
-     :param image_info: ImageUpdateModel: отримати новий опис для зображення
-     :param db: Сеанс: доступ до бази даних
-     :param current_user: Користувач: отримати поточного користувача, який увійшов у систему
-     :return: Словник з ідентифікатором, описом і деталями оновленого зображення
-     """
+The update_description function updates the description of an image.
+    Args:
+        image_id (int): The id of the image to be updated.
+        db (Session, optional): SQLAlchemy Session. Defaults to Depends(get_db).
+        current_user (User, optional): User object for currently logged in user. Defaults to Depends(auth_service.get_current_user).
 
+:param image_id: int: Identify the image to be updated
+:param image_info: ImageUpdateModel: Get the description of the image
+:param db: Session: Get the database session
+:param current_user: User: Get the user who is currently logged in
+:return: A dictionary with the id, description and detail of the image
+
+    """
     user_image = await images.update_image(db, image_id, image_info, current_user)
     return {"id": user_image.id, "description": user_image.description, "detail": "Image was successfully updated"}
 
@@ -140,16 +139,18 @@ async def update_description(image_id: int, image_info: ImageUpdateModel, db: Se
 async def add_tag(image_id, body: ImageAddTagModel = Depends(), db: Session = Depends(get_db),
                   current_user: User = Depends(auth_service.get_current_user)):
     """
-     Функція add_tag додає тег до зображення.
-         Функція приймає такі параметри:
-             - image_id: ідентифікатор зображення, яке позначається тегом.
-             - тіло: об'єкт JSON, що містить назву та опис тегу. Це підтверджується ImageAddTagModel().
-     :param image_id: Визначте зображення, яке потрібно оновити
-     :param body: ImageAddTagModel: отримати тег із тіла запиту
-     :param db: Сеанс: отримати сеанс бази даних
-     :param current_user: Користувач: отримати поточного користувача з бази даних
-     :return: Словник із ідентифікатором зображення, тегами та детальним повідомленням
-     """
+The add_tag function adds a tag to an image.
+    The function takes in the following parameters:
+        - image_id: The id of the image that is being tagged.
+        - body: A JSON object containing the tag name and value, as well as a description of what it means.
+
+:param image_id: Identify the image to be updated
+:param body: ImageAddTagModel: Validate the request body
+:param db: Session: Get the database session
+:param current_user: User: Get the user information from the token
+:return: A dictionary with the updated image id, tags and a detail message
+
+    """
     image, details = await images.add_tag(db, image_id, body, current_user)
     return {"id": image.id, "tags": image.tags, "detail": "Image was successfully updated." + details}
 
@@ -158,12 +159,14 @@ async def add_tag(image_id, body: ImageAddTagModel = Depends(), db: Session = De
 async def delete_image(id: int, db: Session = Depends(get_db),
                        current_user: User = Depends(auth_service.get_current_user)):
     """
-     Функція delete_image видаляє зображення з бази даних.
-         Функція приймає user_id і image_id і повертає словник з інформацією про видалене зображення.
-     :param id: int: Вкажіть ідентифікатор зображення, яке потрібно видалити
-     :param db: Сеанс: передає сеанс бази даних функції
-     :param current_user: Користувач: отримати поточного користувача з бази даних
-     :return: Словник із зображенням і повідомленням про те, що його видалено
-     """
+The delete_image function deletes an image from the database.
+    The function takes in a user_id and an image_id, and returns a dictionary with the deleted image's information.
+
+:param id: int: Specify the id of the image that is to be deleted
+:param db: Session: Get the database session
+:param current_user: User: Get the current user from the database
+:return: A dictionary with the deleted image and a message
+
+    """
     user_image = await images.delete_image(db, id, current_user)
     return {"image": user_image, "detail": "Image was successfully deleted"}
