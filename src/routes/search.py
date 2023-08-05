7302ce86fdd76319c67382adecc7d7533d9a0405
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-08-05 20:59:49 +0200

    Update search.py

--- src/routes/search.py
+++ src/routes/search.py
@@ -23,18 +23,19 @@ async def get_image_by_user_id(user_id: int = Path(ge=1),
                                db: Session = Depends(get_db),
                                current_user: User = Depends(auth_service.get_current_user)):
     """
-     Функція get_image_by_user_id повертає список зображень, які пов’язані з user_id.
-         Функція приймає необов’язкові параметри пропуску та обмеження для керування розбивкою на сторінки, а також необов’язковий filter_type
-         параметр, щоб контролювати, чи потрібно фільтрувати повернуті зображення за датою (d) або (-d).
-         Якщо тип фільтра не вказано, за замовчуванням фільтрується за датою.
-     :param user_id: int: отримати зображення конкретного користувача
-     :param skip: int: Пропустити певну кількість зображень
-     :param limit: int: обмежити кількість зображень, що повертаються
-     :param filter_type: str: фільтрувати зображення за датою, оцінками "подобається" чи коментарями
-     :param db: Сеанс: отримати сеанс бази даних
-     :param current_user: Користувач: отримати поточного користувача
-     :return: Список зображень, які завантажив користувач
-     """
+The get_image_by_user_id function returns a list of images that are associated with the user_id.
+    The function takes in an optional skip and limit parameter to control pagination, as well as an
+    optional filter_type parameter to control how the images are sorted. The default value for filter_type is 'd' which sorts by date.
+
+:param user_id: int: Get the images of a specific user
+:param skip: int: Skip the first n images
+:param limit: int: Limit the number of images returned
+:param filter_type: str: Filter the images by date or popularity
+:param db: Session: Get the database session
+:param current_user: User: Get the user object of the currently logged in user
+:return: A list of images
+
+    """
     images = await get_img_by_user_id(user_id, skip, limit, filter_type, db, current_user)
     return images
 
@@ -46,18 +47,21 @@ async def search_images_by_tag(skip: int = 0, limit: int = 10,
                                db: Session = Depends(get_db),
                                user: User = Depends(auth_service.get_current_user)):
     """
-     Функція search_images_by_tag шукає зображення за тегом.
-         Аргументи:
-             skip (int): кількість зображень, які потрібно пропустити в результатах пошуку. За замовчуванням 0.
-             limit (int): Максимальна кількість зображень для повернення в результатах пошуку. За замовчуванням 10, максимум 100.
-             search_tag (str): Рядок, що містить один або кілька тегів, розділених комами, наприклад, &quot;собака&quot; або &quot;собака,кішка&quot;. Якщо теги не надано, буде повернено всі записи зображень, незалежно від їх пов’язаних тегів.
-     :param skip: int: Пропустити перші n зображень
-     :param limit: int: обмежити кількість зображень, що повертаються
-     :param search_tag: str: Пошук зображень за тегом
-     :param filter_type: str: Визначити сортування зображень за датою 'd' чи '-d' (зворотне сортування)
-     :param db: Сеанс: отримати сеанс бази даних
-     :param user: Користувач: отримати поточного користувача
-     :return: Список об’єктів зображення
-     """
+The search_images_by_tag function searches for images by tag.
+    It takes in a skip, limit, search_tag and filter_type as parameters.
+    The skip parameter is the number of items to be skipped before returning results.
+    The limit parameter is the maximum number of items to return after skipping a given amount of results (skip).
+    The search_tag parameter is used to find images with tags that match this string value.
+    Finally, the filter type determines whether or not we want only public or private images returned.
+
+:param skip: int: Skip the first n images
+:param limit: int: Limit the number of images returned
+:param search_tag: str: Search for images with a specific tag
+:param filter_type: str: Filter the images by date or distance
+:param db: Session: Get the database session
+:param user: User: Check if the user is logged in
+:return: A list of images
+
+    """
     images = await find_image_by_tag(skip, limit, search_tag, filter_type, db, user)
     return images
